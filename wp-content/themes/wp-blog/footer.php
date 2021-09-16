7e15aac88c79183e80148989bceba4f45088c81f
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-16 14:26:44 +0530

    footer

--- wp-content/themes/wp-blog/footer.php
+++ wp-content/themes/wp-blog/footer.php
@@ -1,3 +1,52 @@
-    <?php wp_footer(); ?>
+
+
+    <footer class="site-footer footer_style1 bg-dark f14 text-center">
+        <div class="footer_top pt_60 pb_20">
+            <div class="container">
+                <div class="row">
+                  <div class="col-12 col-sm-6 col-md-4 pl_60">
+                       <h3 class="text-secondary f18 lh22 mb_20 text-uppercase">Social</h3>
+                       <ul class="menu">
+                          <li><a href="#">Neque atque magnam.</a></li>
+                          <li><a href="#">Neque atque magnam.</a></li>
+                          <li><a href="#">Neque atque magnam.</a></li>
+                          <li><a href="#">Neque atque magnam.</a></li>
+                          <li><a href="#">Neque atque magnam.</a></li>
+                          <li><a href="#">Neque atque magnam.</a></li>
+                        </ul>
+                  </div>
+                   <div class="col-12 col-sm-6 col-md-4">
+                      <p><img class="footer_logo" src="https://picsum.photos/id/475/300/100" alt="" /></p>
+                      <p>Our website is specially designed for the beginner. Who's looking for some good program from basic to advanced.</p>
+                  </div>
+                  <div class="col-12 col-sm-6 col-md-4 pl_50">
+                  <h3 class="text-secondary f18 lh22 mb_20 text-uppercase">Posts</h3>
+                      <ul class="menu">
+                        <li><a href="#">Neque atque magnam.</a></li>
+                        <li><a href="#">Neque atque magnam.</a></li>
+                        <li><a href="#">Neque atque magnam.</a></li>
+                        <li><a href="#">Neque atque magnam.</a></li>
+                        <li><a href="#">Neque atque magnam.</a></li>
+                        <li><a href="#">Neque atque magnam.</a></li>
+                      </ul>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer_bottom border-top pt_10 pb_0 f12 text-center">
+            <div class="container">
+                <div class="row">
+                     <div class="col">
+                         <p class="f12 lh18 mb_10">&copy; 2021 WP-Blog. All Rights Reserved.</p>
+                     </div>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+
+  <a href="#" class="gotoparrow"><i class="fas fa-arrow-up"></i></a>
+  <?php wp_footer(); ?>
   </body>
 </html>
