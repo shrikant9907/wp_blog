fb38c2d3bc8396ff6f33cccaaf492fb9837d34d1
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-16 12:04:26 +0530

    register nav menus location

--- wp-content/themes/wp-blog/inc/core/theme-functions.php
+++ wp-content/themes/wp-blog/inc/core/theme-functions.php
@@ -3,3 +3,10 @@
 * It will enable the thumbnail support for posts.
 */ 
 add_theme_support( 'post-thumbnails' );
+
+/*  
+* Register Navigation Menu
+*/
+register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'wp-blog' ),
+) );
