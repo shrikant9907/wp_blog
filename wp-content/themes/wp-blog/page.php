a90837d10a074853b0dc61edc659fefcf521e92a
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-16 17:11:08 +0530

    page file updates

--- wp-content/themes/wp-blog/page.php
+++ wp-content/themes/wp-blog/page.php
@@ -1,2 +1,19 @@
 <?php get_header(); ?> 
+<h1 class="text-center bg-primary mb-5 text-white ptb_40 mont_serrat f30 lh32"><?php the_title(); ?></h1>
+<section class="page-section bg-light common-section-ui">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php 
+					if(have_posts()): 
+						while(have_posts()): 
+							the_post(); 
+							the_content(); 
+						endwhile; 
+					endif; 
+				?>
+			</div>
+		</div>
+	</div>
+</section>
 <?php get_footer();  
